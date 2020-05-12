d959c825851cc278f1fa3d4551e1c2b31a334f55
«Pavel <Mr.Sawv@gmail.com>
2020-05-13 00:25:23 +0300

    feat: Добовление адаптива для попапа

--- script.js
+++ script.js
@@ -7,3 +7,38 @@ let buttonEdit = document.querySelector('.Profile__Edit-button');
     }
   
     buttonEdit.addEventListener('click', showClick);
+
+let close = document.querySelector('.popup__vector');
+
+function closePopup(){
+    document.querySelector('.popup').style.background = 'rgba(0,0,0,0)';
+    document.querySelector('.popup').style.position = 'initial';
+    document.querySelector('.popup__container').style.display = 'none';
+}
+
+close.addEventListener('click', closePopup);
+
+
+//let formElement = document.querySelector('.popup__button')// Воспользуйтесь методом querySelector()
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+//function formSubmitHandler (evt) {
+   // evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+
+    // Находим поля формы в DOM
+   // let nameInput = // Воспользуйтесь инструментом .querySelector()
+    //let jobInput = // Воспользуйтесь инструментом .querySelector()
+
+    // Получите значение полей из свойства value
+
+    // Выберите элементы, куда должны быть вставлены значения полей
+
+    // Вставьте новые значения с помощью textContent
+//}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+//formElement.addEventListener('submit', formSubmitHandler);
