002ca4994f522ca028dc0d6805a99ad3cf68fee5
«Pavel <Mr.Sawv@gmail.com>
2020-05-14 20:41:44 +0300

    fix: исправлены некоторые ошибки связанные с попапом

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let buttonEdit = document.querySelector('.Profile__Edit-button');
 
     function showClick () {
     document.querySelector('.popup').style.background = 'rgba(0,0,0,.6)';
-    document.querySelector('.popup').style.position = 'absolute';
+    document.querySelector('.popup').style.position = 'fixed';
     document.querySelector('.popup__container').style.display = 'flex';
     }
   
