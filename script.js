378ba13b1a8b19aa5433a91da6bea00ad19ce2c0
«Pavel <Mr.Sawv@gmail.com>
2020-05-17 16:43:27 +0300

    hotfix: исправление ошибок

--- script.js
+++ script.js
@@ -1,52 +1,32 @@
 let buttonEdit = document.querySelector('.profile__edit-button');
-
-    function showClick () {
-    document.querySelector('.popup').style.background = 'rgba(0,0,0,.6)';
-    document.querySelector('.popup').style.position = 'fixed';
-    document.querySelector('.popup__container').style.display = 'flex';
-    }
-  
-    buttonEdit.addEventListener('click', showClick);
-
+let formPopup =  document.querySelector('.popup');
+let nameInput = document.querySelector('.popup__input-name');
+let jobInput = document.querySelector('.popup__input-job');
+let name = document.querySelector('.profile__title');
+let Input = document.querySelector('.profile__subtitle');
+let save =  document.querySelector('.popup__button');
+let formElement = document.querySelector('.popup__container');
 let close = document.querySelector('.popup__vector');
 
+
+function showClick () {
+    formPopup.classList.add('popup__opened');
+}
+  
+buttonEdit.addEventListener('click', showClick);
+
 function closePopup(){
-    document.querySelector('.popup').style.background = 'rgba(0,0,0,0)';
-    document.querySelector('.popup').style.position = 'initial';
-    document.querySelector('.popup__container').style.display = 'none';
+    formPopup.classList.remove('popup__opened');
 }
 
 close.addEventListener('click', closePopup);
 
-
-
-
-
-
-let formElement = document.querySelector('.popup');
-
  function formSubmitHandler (evt) {
     evt.preventDefault();
-
-    let nameInput = document.querySelector('.popup__input-name');
-    let jobInput = document.querySelector('.popup__input');
-
-    let name = document.querySelector('.profile__title');
-    let Input = document.querySelector('.profile__subtitle');
-
     name.textContent = nameInput.value;
     Input.textContent = jobInput.value;
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
 
-
-let save =  document.querySelector('.popup__button');
-
-function savePopup(){
-    document.querySelector('.popup').style.background = 'rgba(0,0,0,0)';
-    document.querySelector('.popup').style.position = 'initial';
-    document.querySelector('.popup__container').style.display = 'none';
-}
-
-save.addEventListener('click', savePopup);
+save.addEventListener('click',closePopup);
