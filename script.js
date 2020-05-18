8b121a23f84568beb3f9ffc4223324617ee17eef
«Pavel <Mr.Sawv@gmail.com>
2020-05-18 03:26:09 +0300

    style: Изменеия button и названий фотографий

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let buttonEdit = document.querySelector('.profile__edit-button');
-let formPopup =  document.querySelector('.popup');
-let nameInput = document.querySelector('.popup__input-name');
-let jobInput = document.querySelector('.popup__input-job');
-let name = document.querySelector('.profile__title');
-let Input = document.querySelector('.profile__subtitle');
-let save =  document.querySelector('.popup__button');
-let formElement = document.querySelector('.popup__container');
-let close = document.querySelector('.popup__vector');
+const buttonEdit = document.querySelector('.profile__edit-button');
+const formPopup =  document.querySelector('.popup');
+const nameInput = document.querySelector('.popup__input_name');
+const jobInput = document.querySelector('.popup__input_job');
+const name = document.querySelector('.profile__title');
+const Input = document.querySelector('.profile__subtitle');
+const save =  document.querySelector('.popup__button');
+const formElement = document.querySelector('.popup__container');
+const close = document.querySelector('.popup__vector');
 
 
 function showClick () {
