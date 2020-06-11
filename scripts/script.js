f72ba58f5d6e467a5fbaeed54a5492f8402245db
«Pavel <Mr.Sawv@gmail.com>
2020-06-11 13:43:11 +0300

    fix: Исправления в js коде

--- scripts/script.js
+++ scripts/script.js
@@ -7,14 +7,14 @@ const input = document.querySelector('.profile__subtitle');
 const save =  document.querySelector('.popup__button');
 const formElement = document.querySelector('.popup__container');
 const close = document.querySelector('.popup__vector');
-const pluse = document.querySelector('.profile__button-add');
-const pluseForm =  document.querySelector('.popup__pluse'); 
+const plus = document.querySelector('.profile__button-add');
+const plusForm =  document.querySelector('.popup__pluse'); 
 const elements = document.querySelector('.elements');
 const elementTemplate = document.querySelector('.element__template').content;
-const closePlusebutton = document.querySelector('.popup__vector-pluse');
-const inputPluseName = document.querySelector('.popup__input-pluse');
-const inputPluseLink = document.querySelector('.popup__input-pllink');
-const formAddpluse = document.querySelector('.popup__pluse-container');
+const closePlusButton = document.querySelector('.popup__vector-pluse');
+const inputPlusName = document.querySelector('.popup__input-pluse');
+const inputPlusLink = document.querySelector('.popup__input-pllink');
+const formAddPlus = document.querySelector('.popup__pluse-container');
 const elementImgActive = document.querySelector('.element__image-active');
 const elementTextActive = document.querySelector('.element__text-active');
 const elementOpened =  document.querySelector('.element__opened');
@@ -49,35 +49,42 @@ const initialCards = [
     }
 ];
 
-function openPopup(){
-    formPopup.classList.toggle('popup_opened');
-}
 
-function showClick () {
-    openPopup();
-    nameInput.value = name.textContent
-    jobInput.value =  input.textContent
-}
+function togglePopup(popup) {
+    popup.classList.toggle('popup_opened');
+  }
 
-buttonEdit.addEventListener('click', showClick);
-close.addEventListener('click', openPopup);
+  const getToggleForPopup = (popup) => { 
+    return function() {
+      togglePopup(popup)
+    }
+  }
+  
+  const elementPlusHandler = getToggleForPopup(plusForm);
+  plus.addEventListener('click', elementPlusHandler) 
 
+  const elementPlusHandlerClose = getToggleForPopup (plusForm); 
+  closePlusButton.addEventListener('click', elementPlusHandlerClose); 
+
+    const elementProfileHandler = () => {
+            togglePopup(formPopup);
+            nameInput.value = name.textContent;
+            jobInput.value = input.textContent;
+      }
+    
+    buttonEdit.addEventListener('click', elementProfileHandler); 
+
+  const elementProfileHandlerClose = getToggleForPopup(formPopup); 
+  close.addEventListener('click', elementProfileHandlerClose); 
 
  function formSubmitHandler (evt) {
     evt.preventDefault();
     name.textContent = nameInput.value;
     input.textContent = jobInput.value;
-    openPopup();
+    elementProfileHandlerClose();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
-
-function openPlus(){
-    pluseForm.classList.toggle('popup_opened');
-}
-
-pluse.addEventListener('click', openPlus);
-closePlusebutton.addEventListener('click', openPlus);
 
 function renderTemplate(template, elements){
     elements.prepend(template);
@@ -123,19 +130,20 @@ function getCard(item){
  });
 
 
- formAddpluse.addEventListener('submit', function(evt){  
+ formAddPlus.addEventListener('submit', function(evt){  
     evt.preventDefault();
     const object = {
-    link: inputPluseLink.value,
-    name: inputPluseName.value
+    link: inputPlusLink.value,
+    name: inputPlusName.value
 
       };
 
-      inputPluseName.value = '';
-      inputPluseLink.value = '';
+      inputPlusName.value = '';
+      inputPlusLink.value = '';
 
-      openPlus();
       const card = getCard(object) 
       renderTemplate(card , elements) 
+      elementPlusHandlerClose();
 });
  
+
