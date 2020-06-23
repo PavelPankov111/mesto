16411f572a62557c524bf19af976e693e8d765f6
«Pavel <Mr.Sawv@gmail.com>
2020-06-23 23:42:29 +0300

    gffgedtjrgv

--- scripts/initialCards.js
+++ scripts/initialCards.js
@@ -2,27 +2,27 @@
 
 const initialCards = [
     {
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+        name: 'СПБУТУИЭ',
+        link: 'https://www.restate.ru/attachment/adcc07f02d412fedd341cb1dbce3e28f627ce68c/proportional/1600x1131/AF1QipNnt9n9z1M9Sj4QjyK71k87ueDB_jPQFLp4bQqE%3Ds1600-w1600'
     },
     {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+        name: 'Проспект Большевиков',
+        link: 'https://i02.fotocdn.net/s117/f860e5831a04f46e/public_pin_l/2661720712.jpg'
     },
     {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+        name: 'Новочеркасская',
+        link: 'https://avatars.mds.yandex.net/get-zen_doc/1118263/pub_5db8793f5d6c4b00ae8d8f0e_5db8951abd639600b176e0ec/scale_1200'
     },
     {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+        name: 'Летний сад',
+        link: 'https://puzzleit.ru/files/puzzles/137/136620/_original.jpg'
     },
     {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+        name: 'Улица Белышева',
+        link: 'https://cdn.mlsn.ru/photo/sale/110/11079268/large/4513c8e46582d355640c36c0d8904992.jpg'
     },
     {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+        name: 'Санкт-Петербург',
+        link: 'https://avatars.mds.yandex.net/get-pdb/1874991/bc40701c-e50b-4182-857f-5cea9f43c524/s1200?webp=false'
     }
 ];
